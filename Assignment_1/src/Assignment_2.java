7e605a51f296ebbc6dc106a49a6dc484b5c6ec62
lobhas99 <lobhas0@gmail.com>
2023-10-21 15:20:56 +0530

    assignment1-Q2

--- Assignment_1/src/Assignment_2.java
+++ Assignment_1/src/Assignment_2.java
@@ -1,10 +1,30 @@
 import java.util.Scanner;
 
-public class Assignment_2 {
+public class Assignment_2
+{
 
-	public static void main(String[] args) {
+public static void main(String[] args)
+	{
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("Enter the First No");
+		if (sc.hasNextDouble())
+		{
+			double num1 = sc.nextDouble();
+			{
+				System.out.println("Enter the Second No");
+				if (sc.hasNextDouble())
+				{
+					double num2 = sc.nextDouble();
+					double average = (num1 + num2) / 2;
+					System.out.println("The Average is :" + average);
+				}
+				else
+				{
+					System.out.println("The No is not double");
+				}
+			}
+		}
+		else
+			System.out.println("The No is not double");
 	}
-
 }
